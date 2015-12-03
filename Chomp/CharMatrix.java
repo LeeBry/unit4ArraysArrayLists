4353b9a46f5760af99bcd62fa6e849aee9f352a6
LeeBry <bylee@NN123-HA-06.HS.sd203.org>
2015-12-03 14:10:19 -0600

    a
    b

--- Chomp/CharMatrix.java
+++ Chomp/CharMatrix.java
@@ -4,6 +4,7 @@ public class CharMatrix
 {
   // Instance variables:
   private char grid[][];
+  private char newFillGrid[][];
   // Constructor: creates a grid with dimensions rows, cols,
   // and fills it with spaces
   public CharMatrix(int rows, int cols)
@@ -13,7 +14,7 @@ public class CharMatrix
     {
         for(int j=0; j<grid[i].length; j++)
         {
-            grid[i][j]='';
+            grid[i][j]=' ';
         }
     }
   }
@@ -22,38 +23,67 @@ public class CharMatrix
   // and fills it with the fill  character
   public CharMatrix(int rows, int cols, char fill)
   {
-    ...
+    this.newFillGrid= new char [rows][cols];
+      for(int i=0; i<newFillGrid.length;i++)
+    {
+        for(int j=0; j<newFillGrid[i].length; j++)
+        {
+            newFillGrid[i][j]=fill;
+        }
+    }
   }
 
   // Returns the number of rows in grid
   public int numRows()
   {
-    ...
+    int rowCount=0;
+      for(int i=0; i<grid.length;i++)
+    {
+        rowCount++;
+    }
+    return rowCount;
   }
 
   // Returns the number of columns in grid
   public int numCols()
   {
-    ...
+    int rowCols=0;
+      for(int i=0; i<1;i++)
+    {
+        for(int j=0; j<grid[i].length; j++)
+        {
+            rowCols++;
+        }
+    }
+    return rowCols;
   }
 
   // Returns the character at row, col location
   public char charAt(int row, int col)
   {
-    ...
+    char atRowCol=grid[row][col];
+    return atRowCol;
+    
   }
 
   // Sets the character at row, col location to ch
   public void setCharAt(int row, int col, char ch)
   {
-    ...
+    grid[row][col]=ch;
   }
 
   // Returns true if the character at row, col is a space,
   // false otherwise
   public boolean isEmpty(int row, int col)
   {
-    ...
+    if( grid[row][col]==' ')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
   }
 
   // Fills the given rectangle with fill  characters.
@@ -61,7 +91,13 @@ public class CharMatrix
   // lower right corner of the rectangle.
   public void fillRect(int row0, int col0, int row1, int col1, char fill)
   {
-    ...
+        for(int i=0; i<grid.length;i++)
+    {
+        for(int j=0; j<grid[i].length; j++)
+        {
+            
+        }
+    }
   }
 
   // Fills the given rectangle with SPACE characters.
@@ -69,18 +105,45 @@ public class CharMatrix
   // lower right corner of the rectangle.
   public void clearRect(int row0, int col0, int row1, int col1)
   {
-    ...
+    
   }
 
   // Returns the count of all non-space characters in row 
   public int countInRow(int row)
   {
-    ...
+    int rowCount=0;
+
+    for(int i=0; i<grid.length;i++)
+    {
+        for(int j=0; j<grid[i].length; j++)
+        {
+         if (grid[row][j]==' ')
+        {
+            rowCount++;
+        }
+        }
+    }
+    
+    return rowCount;
   }
 
   // Returns the count of all non-space characters in col 
   public int countInCol(int col)
   {
-    ...
+    
+    int rowCol=0;
+
+    for(int i=0; i<grid.length;i++)
+    {
+        for(int j=0; j<grid[i].length; j++)
+        {
+         if (grid[i][col]==' ')
+        {
+            rowCol++;
+        }
+        }
+    }
+    
+    return rowCol;
   }
 }
