abee39adb9776c3ffde5e00ffbfe06b1ced877fe
LeeBry <bylee@NN123-HA-06.HS.sd203.org>
2015-12-16 13:55:48 -0600

    Done
    Done

--- SignalsInNoiseLab/RadarTest.java
+++ SignalsInNoiseLab/RadarTest.java
@@ -51,8 +51,32 @@ public class RadarTest
         Radar radar= new Radar(10,10);
         // choose monster location
         radar.setMonsterLocation(5,5);
-        // do the scan test
-        assertEquals (10,radar.getNumRows());
+        
+        // do the scan test a lot
+        for( int i=0; i<30; i++)
+        {
+            radar.scan();
+        }
+        radar.getAccumulatedDetection(5,5);
+        assertEquals(10, radar.getNumRows());
+        assertEquals(10, radar.getNumCols());
     }
     
+    @Test
+    public void testInitialState2()
+    {
+        Radar radar= new Radar(100,100);
+        // choose monster location
+        radar.setMonsterLocation(13,42);
+        
+        // do the scan test a lot
+        for( int i=0; i<30; i++)
+        {
+     
+            radar.scan();
+        }
+        radar.getAccumulatedDetection(13,42);
+        assertEquals(100, radar.getNumRows());
+        assertEquals(100, radar.getNumCols());
+    }
 }
